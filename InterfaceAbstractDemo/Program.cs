f4122e1e0e7bc4792dd99c52760e69de5c892ebf
Oğuz Kağan Koç <oguzkagan.koc@bilgeadamakademi.com>
2022-06-24 09:22:11 +0300

    Add project files.

---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Adapters;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+using System;
+
+namespace InterfaceAbstractDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new StarbucksCustomerManager(
+                new MernisServiceAdapter());
+            customerManager.Save(new Customer { 
+                DateofBirth = new System.DateTime(1985, 1, 6), 
+                FirstName = "Engin", 
+                LastName = "Demiroğ",
+                NationalityId = "28861499108" 
+            });
+            Console.ReadLine();
+        }
+    }
+}
